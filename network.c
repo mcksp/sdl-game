18495093f55c083beb49c324a6f2be9ed6e5b61d
maciejspychala <mac.spychala@gmail.com>
2016-12-20 09:10:45 +0100

    save player keys_down to one int

--- network.c
+++ network.c
@@ -1,4 +1,6 @@
 #include "network.h"
+#define TRUE 1
+#define FALSE 0
 
 struct sockaddr_in server_sock_addr() {
     struct sockaddr_in serv_addr;
@@ -36,5 +38,53 @@ int compare_addr(struct sockaddr_in *a, struct sockaddr_in *b) {
         return 1;
     } else {
         return 0;
+    }
+}
+
+int16_t key_state_from_player(struct Player *player) {
+    int16_t key_state = 0;
+    if (player->left_key) {
+        key_state = key_state | 0x01;
+    }
+    if (player->right_key) {
+        key_state = key_state | 0x02;
+    }
+    if (player->up_key) {
+        key_state = key_state | 0x04;
+    }
+    if (player->down_key) {
+        key_state = key_state | 0x08;
+    }
+    if (player->attack_key) {
+        key_state = key_state | 0x10;
+    }
+    return key_state;
+}
+
+void player_from_key_state(struct Player *player, int16_t key_state) {
+    if (key_state & 0x01) {
+        player->left_key = TRUE;
+    } else {
+        player->left_key = FALSE;
+    }
+    if (key_state & 0x02) {
+        player->right_key = TRUE;
+    } else {
+        player->right_key = FALSE;
+    }
+    if (key_state & 0x04) {
+        player->up_key = TRUE;
+    } else {
+        player->up_key = FALSE;
+    }
+    if (key_state & 0x08) {
+        player->down_key = TRUE;
+    } else {
+        player->down_key = FALSE;
+    }
+    if (key_state & 0x10) {
+        player->attack_key = TRUE;
+    } else {
+        player->attack_key = FALSE;
     }
 }
--- network.h
+++ network.h
@@ -4,9 +4,14 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <objects.h>
 
 struct sockaddr_in server_sock_addr();
 struct sockaddr_in client_sock_addr();
 int addr_pos_in_tab(struct sockaddr_in new_addr, struct sockaddr_in old_addr_tab[], int size);
 int compare_addr(struct sockaddr_in *a, struct sockaddr_in *b);
+int16_t key_state_from_player(struct Player *player);
+void player_from_key_state(struct Player *player, int16_t key_state);
+
 #endif
